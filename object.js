24cd78d0e47f4c8759415beb790405f62c7ffc9d
TyperGuy <ray.developper@gmail.com>
2022-01-23 23:10:58 +0100

    first commit

---
+++ object.js
@@ -0,0 +1,22 @@
+const employers = [
+  {
+    NF:1,
+    firstName: "José",
+    lastName: "Tony",
+    email: "jose.tony@gmail.com"
+  },
+ {
+    NF:2,
+    firstName: "Raimundo",
+    lastName: "Tony",
+    email: "ray.tony@gmail.com"
+  },
+  {
+    NF:3,
+    firstName: "Anderson",
+    lastName: "Kennedy",
+    email: "anderson.kedriny@gmail.com"
+  }
+]
+
+console.table(employers);
